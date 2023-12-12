6130c874bd2f44b90094ee3944fe75e0491566d2
Victor Gutierrez <victor.gutierrez@wundermanthompson.com>
2023-12-12 16:09:24 -0300

    limpiando proyecto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <Layout>
     <h1 className="text-4xl text-center text-blue-500 my-5">Hello Next.js 👋</h1>
-    <p className="text-center">
-      <Link href="/about" className="text-red-500 hover:underline">About</Link>
-    </p>
   </Layout>
 )
 
