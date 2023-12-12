695d16c070edee408a73b1e727faa9f28aa1de83
Victor Gutierrez <victor.gutierrez@wundermanthompson.com>
2023-12-12 15:56:49 -0300

    configurando tailwind para ser utilizado en toda la app

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
+    <h1 className="text-4xl text-center text-blue-500 my-5">Hello Next.js 👋</h1>
+    <p className="text-center">
+      <Link href="/about" className="text-red-500 hover:underline">About</Link>
     </p>
   </Layout>
 )
