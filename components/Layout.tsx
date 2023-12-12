e52a4147d32c1d62a384dec9eaa4c817ae792709
Victor Gutierrez <victor.gutierrez@wundermanthompson.com>
2023-12-12 18:20:22 -0300

    agregando logo, ícono y primer componente header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,29 +1,24 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import MainHeader from './MainHeader'
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = 'Gutiérrez, Víctor' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
+      <link rel="shortcut icon" href="logo.ico" type="image/x-icon"></link>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link>
-      </nav>
-    </header>
+    <MainHeader />
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
+    {/* componente footer */}
   </div>
 )
 
---
+++ components/MainHeader.tsx
@@ -0,0 +1,15 @@
+
+const MainHeader = () => (
+  <header>
+    {/* logo init */}
+    <img src="logo.png" alt="logo" className="flex w-40 border-2 rounded-lg shadow-xl drop-shadow-xl p-4 m-4" />
+    {/* logo end */}
+    {/* navBar init */}
+    {/* navBar end */}
+    {/* botones init */}
+
+    {/* botones end */}
+  </header>
+)
+
+export default MainHeader
